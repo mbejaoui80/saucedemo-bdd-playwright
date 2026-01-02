0c732accc9aadf170b3f29bd1e4d34f0b64bc021
mbejaoui80 <testautomationmbe@gmail.com>
2026-01-02 12:55:57 +0100

    Ajout du rapport JSON Cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,13 +1,10 @@
-// cucumber.js
 module.exports = {
   default: {
-    // Où sont les fichiers Gherkin (.feature)
     paths: ['features/**/*.feature'],
-    // Où sont les scripts JS (.js)
     require: ['step-definitions/**/*.js'],
-    // Format du rapport de test dans la console
-    format: ['progress-bar', 'html:cucumber-report.html'],
-    // Pour gérer les modules modernes
-    import: [], 
+    format: [
+      'progress-bar', // Pour voir la barre dans la console
+      'json:reports/cucumber_report.json' // Pour générer le fichier pour Jenkins
+    ]
   }
-}
+};
